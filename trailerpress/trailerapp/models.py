a1132835b4f08f4810aee05b8284966b6dd50fe3
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-12 18:46:18 +0100

    Add models and migration

--- trailerpress/trailerapp/models.py
+++ trailerpress/trailerapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+    __repr__ = __str__
+
+
+class Trailer(models.Model):
+    title = models.CharField(max_length=255)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    __repr__ = __str__
