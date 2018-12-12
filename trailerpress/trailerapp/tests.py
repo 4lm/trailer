88aacf009b4b4cd2082aac71db8e3c8173021dc9
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-12 21:27:27 +0100

    Add model tests

--- trailerpress/trailerapp/tests.py
+++ trailerpress/trailerapp/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Film, Trailer
+from uuid import uuid4
 
-# Create your tests here.
+
+class FilmModelTests(TestCase):
+
+    def test_str(self):
+        '''
+            Test __str__ method of model Film.
+        '''
+        uuid = uuid4()
+        test_film = Film(title=uuid)
+        self.assertEqual(test_film.__str__(), uuid)
+
+
+class TrailerModelTests(TestCase):
+
+    def test_str(self):
+        '''
+            Test __str__ method of model Trailer.
+        '''
+        uuid = uuid4()
+        test_trailer = Trailer(title=uuid)
+        self.assertEqual(test_trailer.__str__(), uuid)
