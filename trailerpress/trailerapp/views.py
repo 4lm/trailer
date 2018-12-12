57cb53d3f758f56f86efc6d151cf66d3525ab70f
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-12 22:40:04 +0100

    Add pagination

--- trailerpress/trailerapp/views.py
+++ trailerpress/trailerapp/views.py
@@ -5,6 +5,8 @@ from .models import Film
 
 class FilmIndexListView(ListView):
     model = Film
+    template_name = 'trailerapp/home.html'
+    context_object_name = 'films'
     paginate_by = 5
 
     def get_context_data(self, **kwargs):
