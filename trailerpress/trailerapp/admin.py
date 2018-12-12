0fac4432a42ba4e3e507d09d1b1208e4bb123f44
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-12 21:27:05 +0100

    Improve styling

--- trailerpress/trailerapp/admin.py
+++ trailerpress/trailerapp/admin.py
@@ -5,11 +5,14 @@ from .models import Film, Trailer
 class TrailerAdmin(admin.ModelAdmin):
     list_display = ['title', 'film']
 
+
 class TrailerInline(admin.StackedInline):
     model = Trailer
 
+
 class FilmAdmin(admin.ModelAdmin):
     inlines = [TrailerInline]
+
 
 admin.site.register(Film, FilmAdmin)
 admin.site.register(Trailer, TrailerAdmin)
