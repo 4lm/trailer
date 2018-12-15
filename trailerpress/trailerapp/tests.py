ddc1431e50e23ac59885181e353d6c1220095a9e
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-15 21:30:47 +0100

    Add FilmIndexListView tests.

--- trailerpress/trailerapp/tests.py
+++ trailerpress/trailerapp/tests.py
@@ -1,9 +1,13 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 from .models import Film, Trailer
+from .views import FilmIndexListView
 from uuid import uuid4
 
 
 class FilmModelTests(TestCase):
+    '''
+        FilmModel tests.
+    '''
 
     def test_str(self):
         '''
@@ -15,6 +19,9 @@ class FilmModelTests(TestCase):
 
 
 class TrailerModelTests(TestCase):
+    '''
+        TrailerModel tests.
+    '''
 
     def test_str(self):
         '''
@@ -23,3 +30,37 @@ class TrailerModelTests(TestCase):
         uuid = uuid4()
         test_trailer = Trailer(title=uuid)
         self.assertEqual(test_trailer.__str__(), uuid)
+
+
+class FilmIndexListViewTests(TestCase):
+    '''
+        FilmIndexListView tests.
+    '''
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_context_of_film_index(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['page_title'], 'TrailerPress')
+
+    def test_film_index_model(self):
+        film_index = FilmIndexListView()
+        self.assertEqual(film_index.model, Film)
+
+    def test_film_index_template_name(self):
+        film_index = FilmIndexListView()
+        self.assertEqual(film_index.template_name, 'trailerapp/home.html')
+
+    def test_film_index_context_object_name(self):
+        film_index = FilmIndexListView()
+        self.assertEqual(film_index.context_object_name, 'films')
+
+    def test_film_index_context_pagination(self):
+        film_index = FilmIndexListView()
+        self.assertEqual(film_index.paginate_by, 5)
+
+    def test_film_index_ordering(self):
+        film_index = FilmIndexListView()
+        self.assertEqual(film_index.ordering[0], 'title')
