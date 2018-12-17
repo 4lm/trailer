a1ce006b514dbd68919d03810261e329406cd6bb
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-17 20:45:47 +0100

    Add API_KEY to settings

--- trailerpress/settings.py
+++ trailerpress/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# TMDB API_KEY
+API_KEY = config('API_KEY', default='')
