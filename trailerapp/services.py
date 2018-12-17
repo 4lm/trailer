f4f4adfe394bb6c05b0d46f80e5dbbb7001483b6
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-17 20:50:26 +0100

    Add playing view, services.py domain logic and wire things up

---
+++ trailerapp/services.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def get_data(api_key: str, language: str, page: int, region: str):
+    url = 'https://api.themoviedb.org/3/movie/now_playing'
+    params = {'api_key': api_key, 'language': language, 'page': page, 'region': region}
+    r = requests.get(url, params=params)
+    data = r.json()
+    counter = data['total_pages']
+    if counter > 5:
+        counter = 5
+    results = []
+    while counter > 0:
+        params = {'api_key': api_key, 'language': language, 'page': counter, 'region': region}
+        r = requests.get(url, params=params)
+        data = r.json()
+        for item in data['results']:
+            results.append(item)
+        counter -= 1
+    if len(results) is not 0:
+        for result in results:
+            url = 'https://api.themoviedb.org/3/movie/' + str(result['id']) + '/videos'
+            params = {'api_key': api_key, 'language': language}
+            r = requests.get(url, params=params)
+            data = r.json()
+            result['videos_results'] = data['results']
+    return results
