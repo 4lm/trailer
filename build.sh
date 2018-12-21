a9cb57c112a8032e6cfe52f4900ed8be09ae9036
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-21 10:26:55 +0100

    Add Node.js

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp -v node_modules/video.js/dist/video-js.min.css trailerapp/static/trailerapp/js/
+cp -v node_modules/video.js/dist/video.min.js trailerapp/static/trailerapp/js/
+cp -v node_modules/video.js/dist/video.min.js trailerapp/static/trailerapp/js/
