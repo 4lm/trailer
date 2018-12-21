b8c3562cc7214c550aa7f4b5c3c9580feac8af31
Alexis Michaltsis <alexis@michaltsis.net>
2018-12-21 23:15:25 +0100

    Add missing map file

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ cp -v node_modules/video.js/dist/video-js.min.css trailerapp/static/trailerapp/j
 cp -v node_modules/video.js/dist/video.min.js trailerapp/static/trailerapp/js/
 cp -v node_modules/video.js/dist/video.min.js trailerapp/static/trailerapp/js/
 cp -v node_modules/is-in-viewport/lib/isInViewport.min.js trailerapp/static/trailerapp/js/
+cp -v node_modules/is-in-viewport/lib/isInViewport.min.js.map trailerapp/static/trailerapp/js/
