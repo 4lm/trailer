d1b0be743b999d180263b402fd9b68ebe609474c
Alexis Michaltsis <alexis@michaltsis.net>
2019-01-06 15:41:28 +0100

    Save profile at user creation and add profile picture

--- trailerapp/urls.py
+++ trailerapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 app_name = 'trailerapp'
@@ -11,3 +13,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
