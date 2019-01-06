dc9782002fd3a8f274adb8a4d191ffcf38b82713
Alexis Michaltsis <alexis@michaltsis.net>
2019-01-06 14:15:51 +0100

    Add profile model

--- trailerapp/models.py
+++ trailerapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Genre(models.Model):
@@ -45,5 +46,15 @@ class Trailer(models.Model):
 
     def __str__(self):
         return self.title
+
+    __repr__ = __str__
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return '{} Profile'.format(self.user.username)
 
     __repr__ = __str__
